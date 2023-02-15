479b2b241539f29b33b3750ca3d027d5422b3165
Haaarimmm <haaarimmm@gmail.com>
2023-02-16 00:42:20 +0900

    0215 maze

--- algorithm/practice/stack/0215_stack_3_addi.py
+++ algorithm/practice/stack/0215_stack_3_addi.py
@@ -1,11 +1,11 @@
 import sys
-
 sys.stdin = open('input.txt', 'r')
 
 def dfs(si, sj):
     st = []
     di = [-1, 1, 0, 0]
     dj = [0, 0, -1, 1]
+
     ci, cj = si, sj     # 기준점
     arr[ci][cj] = '1'   # 방문한 위치를 벽으로 처리
 
@@ -15,8 +15,8 @@ def dfs(si, sj):
             # 미로 범위 내/0 혹은 3(미방문)
             if 0 <= vi < N and 0 <= vj < N and arr[vi][vj] != '1':
                 st.append((ci, cj))   # 부모노드(돌아올 지점) 저장
+                arr[vi][vj] = '1'   # 자식노드 방문처리
                 ci, cj = vi, vj     # 기준점 변경(자식노드 ni, nj)로
-                arr[ci][cj] = '1'   # 자식노드 방문처리
                 break               # 이동 경로 찾자마자 실행
         else:
             if st: # 돌아갈 지점 존재
@@ -24,9 +24,10 @@ def dfs(si, sj):
             else:  # 돌아갈 지점 X
                 break
 
-T = int(input())
-for tc in range(1, T+1):
-    N = int(input())
+T = 10
+for tc in range(T):
+    N = 16
+    tc = int(input())
     arr = [list(input()) for _ in range(N)]
 
     # 시작 노드 찾기
