95b30f5a3cd39c9a83067156dc71b918484a768e
Haaarimmm <haaarimmm@gmail.com>
2023-03-22 23:35:32 +0900

    230322

--- java/APS/src/Main.java
+++ java/APS/src/Main.java
@@ -1,21 +1,18 @@
 import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        HashSet<Integer> h = new HashSet();
 
-        int T = Integer.parseInt(br.readLine());
-
-        for (int i=0; i<T; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            bw.write(A + B + "\n");
+        for (int i=0; i<10; i++){
+            h.add((Integer.parseInt(br.readLine())) % 42);
         }
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(h.size());
+
     }
 }
